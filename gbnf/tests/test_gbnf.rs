ddbe80934791332ac6711234dfdeda5afcbecad2
AsbjornOlling <asbjornolling@gmail.com>
2026-01-25 01:29:32 +0100

    split into three

--- gbnf/tests/test_gbnf.rs
+++ gbnf/tests/test_gbnf.rs
@@ -1,4 +1,4 @@
-use gbnf::{gbnf, Expr, GbnfGrammar, Quantifier};
+use gbnf::{gbnf, GbnfGrammar};
 
 #[test]
 fn test_basic_declarations() {
@@ -8,15 +8,7 @@ fn test_basic_declarations() {
         okabob ::= other
     };
 
-    assert_eq!(grammar.declarations.len(), 3);
-    assert_eq!(grammar.declarations[0].name, "root");
-    assert_eq!(grammar.declarations[1].name, "foobar");
-    assert_eq!(grammar.declarations[2].name, "okabob");
-
-    // Verify GBNF string output
-    assert!(grammar.as_str().contains("root ::= foobar"));
-    assert!(grammar.as_str().contains("foobar ::= \"Abekat\""));
-    assert!(grammar.as_str().contains("okabob ::= other"));
+    insta::assert_debug_snapshot!(grammar);
 }
 
 #[test]
@@ -25,9 +17,7 @@ fn test_single_declaration() {
         single ::= value
     };
 
-    assert_eq!(grammar.declarations.len(), 1);
-    assert_eq!(grammar.declarations[0].name, "single");
-    assert_eq!(grammar.as_str(), "single ::= value");
+    insta::assert_debug_snapshot!(grammar);
 }
 
 #[test]
@@ -36,12 +26,7 @@ fn test_alternation() {
         rule ::= foo | bar | baz
     };
 
-    assert_eq!(grammar.declarations.len(), 1);
-    assert!(matches!(
-        &grammar.declarations[0].expr,
-        Expr::Alternation(alts) if alts.len() == 3
-    ));
-    assert_eq!(grammar.as_str(), "rule ::= foo | bar | baz");
+    insta::assert_debug_snapshot!(grammar);
 }
 
 #[test]
@@ -50,12 +35,7 @@ fn test_sequence() {
         rule ::= "hello" " " "world"
     };
 
-    assert_eq!(grammar.declarations.len(), 1);
-    assert!(matches!(
-        &grammar.declarations[0].expr,
-        Expr::Sequence(items) if items.len() == 3
-    ));
-    assert_eq!(grammar.as_str(), "rule ::= \"hello\" \" \" \"world\"");
+    insta::assert_debug_snapshot!(grammar);
 }
 
 #[test]
@@ -64,32 +44,7 @@ fn test_quantifiers() {
         rule ::= foo? bar+ baz*
     };
 
-    assert_eq!(grammar.declarations.len(), 1);
-    if let Expr::Sequence(items) = &grammar.declarations[0].expr {
-        assert_eq!(items.len(), 3);
-
-        // Check first item is Optional quantifier
-        assert!(matches!(
-            &items[0],
-            Expr::Quantified { quantifier: Quantifier::Optional, .. }
-        ));
-
-        // Check second item is OneOrMore quantifier
-        assert!(matches!(
-            &items[1],
-            Expr::Quantified { quantifier: Quantifier::OneOrMore, .. }
-        ));
-
-        // Check third item is ZeroOrMore quantifier
-        assert!(matches!(
-            &items[2],
-            Expr::Quantified { quantifier: Quantifier::ZeroOrMore, .. }
-        ));
-    } else {
-        panic!("Expected sequence");
-    }
-
-    assert_eq!(grammar.as_str(), "rule ::= foo? bar+ baz*");
+    insta::assert_debug_snapshot!(grammar);
 }
 
 #[test]
@@ -98,12 +53,7 @@ fn test_grouping() {
         rule ::= (foo | bar) baz
     };
 
-    assert_eq!(grammar.declarations.len(), 1);
-    assert!(matches!(
-        &grammar.declarations[0].expr,
-        Expr::Sequence(items) if items.len() == 2
-    ));
-    assert_eq!(grammar.as_str(), "rule ::= (foo | bar) baz");
+    insta::assert_debug_snapshot!(grammar);
 }
 
 #[test]
@@ -113,12 +63,7 @@ fn test_character_range() {
         letter ::= [a-z]
     };
 
-    assert_eq!(grammar.declarations.len(), 2);
-    assert_eq!(grammar.declarations[0].name, "digit");
-    assert_eq!(grammar.declarations[1].name, "letter");
-
-    assert!(grammar.as_str().contains("digit ::= [0-9]"));
-    assert!(grammar.as_str().contains("letter ::= [a-z]"));
+    insta::assert_debug_snapshot!(grammar);
 }
 
 #[test]
@@ -131,12 +76,7 @@ fn test_complex_example() {
         castle ::= "O-O" | "O-O-O"
     };
 
-    assert_eq!(grammar.declarations.len(), 5);
-
-    // Verify we can get the complete GBNF string
-    let gbnf_str = grammar.as_str();
-    assert!(gbnf_str.contains("root ::= chessmove+"));
-    assert!(gbnf_str.contains("castle ::= \"O-O\" | \"O-O-O\""));
+    insta::assert_debug_snapshot!(grammar);
 }
 
 #[test]
@@ -145,10 +85,7 @@ fn test_grammar_string_output() {
         root ::= "hello" " " "world"
     };
 
-    // The grammar should provide a valid GBNF string
-    let expected = "root ::= \"hello\" \" \" \"world\"";
-    assert_eq!(grammar.as_str(), expected);
-    assert_eq!(grammar.gbnf_string, expected);
+    insta::assert_debug_snapshot!(grammar);
 }
 
 #[test]
@@ -157,13 +94,5 @@ fn test_ast_structure() {
         greeting ::= "hello"
     };
 
-    assert_eq!(grammar.declarations.len(), 1);
-    let decl = &grammar.declarations[0];
-    assert_eq!(decl.name, "greeting");
-
-    if let Expr::Characters(s) = &decl.expr {
-        assert_eq!(s, "hello");
-    } else {
-        panic!("Expected Characters expression");
-    }
+    insta::assert_debug_snapshot!(grammar);
 }
